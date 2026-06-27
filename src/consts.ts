c674efe84566119cf0a82baf517fd05c064e8d2b
akoudad <akoudad22karim@gmail.com>
2026-06-27 01:46:52 +0100

    edit: shorten name to akoudad, add linkedin

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Karim Akoudad';
+export const SITE_TITLE = 'Akoudad';
 export const SITE_DESCRIPTION = 'Ideas, opinions, things I make and learn — technical and not.';
